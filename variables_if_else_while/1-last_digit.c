b1a630fdde7ccd8bb5d48ea843efe89647fe4791
sindi702 <hamatajsindi@gmail.com>
2023-02-20 03:04:03 -0800

    cprogramming

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -1,9 +1,10 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
-/** more headers goes there
+/**
+*main - Entry point
 *
-* betty style doc for function main goes there
+*Return: Always 0 (Success)
 */
 int main(void)
 {
