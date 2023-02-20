545e2e98f97a063add607a44443cfcd31e84f32d
sindi702 <hamatajsindi@gmail.com>
2023-02-20 04:00:16 -0800

    cprogramming

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
 /**
-* *  * * main - Entry point
-* *   * *
-* *    * * Return: Always 0 (Success)
-* *     *
-* */
+* main - Entry poit
+*
+*
+*/
 int main(void)
 {
 char letter = 'a';
 while (letter <= 'z')
 {
-if(letter != 'q' && letter != 'e')
+if (letter != 'q' && letter != 'e')
 {
 putchar (letter);
 }
