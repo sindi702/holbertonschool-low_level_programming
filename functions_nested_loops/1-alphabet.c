77f203fa54bae776b000dddd98da2783c3fe296b
sindi702 <hamatajsindi@gmail.com>
2023-02-21 02:23:18 -0800

    alphabet

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * * main - Entry point
+ * *
+ * * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+char letter = 'a';
+while (letter <= 'z')
+{
+_putchar (letter);
+letter++;
+}
+_putchar ('\n');
+}
