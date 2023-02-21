a97b93649f843931e3cdfff1dee89bd8a0cfb288
sindi702 <hamatajsindi@gmail.com>
2023-02-21 03:16:33 -0800

    alphabet

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
-* print_alphabet - main func
+* print_alphabet_x10 - main func
 * Return: Always 0 (Success)
 */
 void print_alphabet_x10(void)
-{ 
+{
 int i;
-for(i=0;i<10;i++)
+for (i = 0 ; i < 10 ; i++)
 {
 char letter = 'a';
 while (letter <= 'z')
