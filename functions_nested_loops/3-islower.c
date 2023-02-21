68146c7feec5357b35d1b1f21b23f30950d4209c
sindi702 <hamatajsindi@gmail.com>
2023-02-21 04:06:53 -0800

    alphabet

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *  * print_alphabet - main func
+ *   * Return: Always 0 (Success)
+ *    */
+int _islower(int c)
+{
+if(c >= 'a' && c <= 'z')
+{
+	_putchar (1);
+}
+else{
+	_putchar (0);
+}
+return (0);
+}
