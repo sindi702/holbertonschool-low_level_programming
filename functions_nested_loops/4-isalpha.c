e1e90529e111e275b522860003db25fedc869bf8
sindi702 <hamatajsindi@gmail.com>
2023-02-21 04:56:51 -0800

    alphabet

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,18 +1,19 @@
 #include "main.h"
 /**
- *  * _islower - main func
- *   * Return: Always 0 (Success)
- *    * @c: The character to print
- *     */
+ *_isalpha - main func
+ * Return: Always 0 (Success)
+ * @c: The character to print
+ */
 int _isalpha(int c)
 {
-	if ((c >= 'a' && c <= 'z' ) || (c >= 'A' && c <= 'Z'))
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 	{
 		return (1);
 	}
 	else
 	{
 		return (0);
+
 		c++;
 }
 return (0);
