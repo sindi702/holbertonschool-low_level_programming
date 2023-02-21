f4ef602d39fc03e0dcfdfbd1c9014b67fb2a58df
sindi702 <hamatajsindi@gmail.com>
2023-02-21 02:50:29 -0800

    alphabet

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* print_alphabet - main func
+* Return: Always 0 (Success)
+*/
+void print_alphabet_x10(void)
+{ 
+int i;
+for(i=0;i<10;i++)
+{
+char letter = 'a';
+while (letter <= 'z')
+{
+_putchar (letter);
+letter++;
+}
+_putchar ('\n');
+}
+}
