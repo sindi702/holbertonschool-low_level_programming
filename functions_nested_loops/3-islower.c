4cdaa69e349632cd264c4b969511efe2662debaf
sindi702 <hamatajsindi@gmail.com>
2023-02-21 04:29:32 -0800

    alphabet

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -5,13 +5,14 @@
  *    */
 int _islower(int c)
 {
-if(c >= 'a' && c <= 'z')
+	if(c >= 'a' && c <= 'z')
 {
-_putchar (1);
+	return (1);
 }
 else
 {
-_putchar (0);
+	return (0);
+	c++;
 }
 return (0);
 }
