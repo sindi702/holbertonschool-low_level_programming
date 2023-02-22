304fc8b28b2be1f6d6504c7563380604f268b3d7
sindi702 <hamatajsindi@gmail.com>
2023-02-22 01:27:12 -0800

    absolute

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,15 +1,22 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * main - check the code
  * print_last_digit - main func
  * Return: Always 0 (Success)
- * @n: The character to print
+ * @num: The character to print
  *
  */
 int print_last_digit(int num)
 {
 	int last_digit = num % 10;
-	_putchar(last_digit);
+	if(last_digit < 0)
+	{
+		last_digit = -last_digit;
+	}
+	else
+	{
+		last_digit = last_digit;
+	}
+	_putchar( '0' + last_digit);
 	return (last_digit);
 }
