7854d185700587cce50a44ace4387ef317fd3fd2
sindi702 <hamatajsindi@gmail.com>
2023-02-22 01:12:56 -0800

    absolute

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -10,6 +10,6 @@
 int print_last_digit(int num)
 {
 	int last_digit = num % 10;
-	printf("The last digit of %d is %d\n", num, last_digit);
-	return last_digit;
+	_putchar(last_digit);
+	return (last_digit);
 }
