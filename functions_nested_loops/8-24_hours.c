8830d9d2518d38d688f631e0126a0150ebce1b15
sindi702 <hamatajsindi@gmail.com>
2023-02-22 01:53:28 -0800

    absolute

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,24 +1,25 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * print_last_digit - main func
+ * jack_bauer - main func
  * Return: Always 0 (Success)
- * @num: The character to print
- *
+ * @hour: The character to print
+ * @minute: The character to print
  */
 void jack_bauer(void) {
 	int hour;
 	int minute;
+	
 	for (hour = 0; hour < 24; hour++)
 	{
 		for (minute = 0; minute < 60; minute++)
 		{
 			putchar(hour / 10 + '0');
-			          putchar(hour % 10 + '0');
-				                putchar(':');
-						putchar(minute / 10 + '0');
-							                putchar(minute % 10 + '0');
-									            putchar('\n');
+			putchar(hour % 10 + '0');
+		        putchar(':');
+			putchar(minute / 10 + '0');
+			putchar(minute % 10 + '0');
+			putchar('\n');
 		}
 	}
 }
