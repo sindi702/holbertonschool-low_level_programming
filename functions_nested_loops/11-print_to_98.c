10ca54865572b81b84250cfe60981e2ad9871565
sindi702 <hamatajsindi@gmail.com>
2023-02-23 00:52:40 -0800

    done

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <unistd.h>
 #include <stdio.h>
 /**
  * print_to_98 - Prints all natural numbers from n to 98
@@ -40,4 +41,5 @@ void print_to_98(int n)
 			}
 		}
 	}
+	printf("\n");
 }
