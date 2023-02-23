5e3778fea433e80c5e1ee761407073ba799e71b9
sindi702 <hamatajsindi@gmail.com>
2023-02-23 05:00:18 -0800

    function

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+#include "main.h"
+/**
+ * isupper - main function
+ * @c: parameter
+ * Return: 1 if uppercase and 0 if false
+ */
+int mul(int a, int b)
+{
+	return (a*b);
+}
