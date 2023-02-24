c5a4d8a6fbf78c370d8ce4b40cbfb681d8c6a552
sindi702 <hamatajsindi@gmail.com>
2023-02-24 02:29:16 -0800

    done

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+#include "main.h"
+/**
+ * print_numbers - main function
+ * @a: first parameter
+ * Return: a 
+ */
+void print_numbers(void)
+{
+	int a ;
+
+       for(a=0 ; a<=9 ;a++)
+       {
+	       _putchar(a);
+       }	       
+	_putchar("\n");
+}
