6186d0d07c855cfa8c65d01702dd31d4b0eaa9d3
sindi702 <hamatajsindi@gmail.com>
2023-02-24 02:57:09 -0800

    done

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+#include "main.h"
+/**
+ *  * print_numbers - main function
+ *   * Return: 1 if upercase
+ *    */
+void print_numbers(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+	{
+		if (a!='2' && a!='4' )
+			_putchar('0' + a);
+		_putchar('\n');
+}
