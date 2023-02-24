0a45ae183381ea2e2460793269402f3ce97ccd1e
sindi702 <hamatajsindi@gmail.com>
2023-02-24 02:42:05 -0800

    done

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -2,15 +2,15 @@
 #include "main.h"
 /**
  * print_numbers - main function
- * Return: 1 if upercase 
+ * Return: 1 if upercase
  */
 void print_numbers(void)
 {
 	int a;
-	
-	for(a = 0 ; a <= 9 ; a++)
+
+	for (a = 0 ; a <= 9 ; a++)
 	{
 	       _putchar('0' + a);
-	}	       
+	}
 	_putchar('\n');
 }
