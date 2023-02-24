99294d7444e53c178f37ec07aab80921c546f6d6
sindi702 <hamatajsindi@gmail.com>
2023-02-24 03:33:16 -0800

    done

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -1,19 +1,19 @@
 #include <unistd.h>
 #include "main.h"
 /**
- * void more_numbers - main function
- * Return: 1 if uppercase and 0 if false
+ * more_numbers - main function
+ * Return: 1
  */
 void more_numbers(void)
 {
 	int i;
 	int j;
 
-	for ( i = 0; i <= 9; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		for(j = 0; j <= 14; j++)
+		for (j = 0; j <= 14; j++)
 		{
-			if ( j >= 10 )
+			if (j >= 10)
 				_putchar(j / 10 + '0');
 			_putchar(j % 10 + '0');
 	}
