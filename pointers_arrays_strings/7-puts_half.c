9a40c3a04f563f24f5afa61472d5ca65da2b6c2c
sindi702 <hamatajsindi@gmail.com>
2023-02-28 04:34:57 -0800

    pointers

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ *
+ *
+ */
+void puts_half(char *str)
+{
+	int i = 0,len;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	len = i;
+
+	for(i = 0; i < len; i++)
+	{
+		if (i > len / 2 && i % 2 == 0)
+			_putchar(str[i]);
+		else if (i >= len / 2 && i % 2 != 0)
+			_putchar(str[i]);
+	}
+	_putchar('\n');
+}
+
