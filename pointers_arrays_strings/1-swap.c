576d32696fa2074bea58619b8f8b81ec30f38a9e
sindi702 <hamatajsindi@gmail.com>
2023-02-28 00:52:34 -0800

    pointers

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -1,10 +1,13 @@
 #include "main.h"
 /**
  * swap_int - main function
+ * @a: The character to print
+ * @b: The character to print
  */
 void swap_int(int *a, int *b)
 {
 	int tmp;
+
 	tmp = *a;
 	*a = *b;
 	*b = tmp;
