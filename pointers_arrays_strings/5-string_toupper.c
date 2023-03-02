1ec41f49869a7ab2a84700c78e5ca71c66df7a68
sindi702 <hamatajsindi@gmail.com>
2023-03-02 12:03:13 -0800

    pointers

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -1,19 +1,18 @@
 #include "main.h"
 #include <string.h>
 /**
- *
+ * string_toupper - main function
+ * Return: returns sum
+ * @string: param
  *
  */
 char *string_toupper(char *s)
 {
-	int i = 0;
-	while (s[i] != '\0')
+	int i;
+	for (i = 0; string[i] != '\0'; i++)
 	{
-		if (s[i] >= 'a' && s[i] <= 'z')
-		{
-			 s[i] = toupper(s[i]);
-		}
-		i++;
+		if (string[i] >= 'a' && string[i <= 'b'])
+			string[i] -= 32;
 	}
-	return s;
+	return (string);
 }
