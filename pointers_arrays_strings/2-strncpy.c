52ccdcaf65332e1aa401277ae2c5deb852950510
sindi702 <hamatajsindi@gmail.com>
2023-03-02 03:07:57 -0800

    pointers

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,17 +1,19 @@
 #include "main.h"
+#include <string.h>
 /**
- *
- *
- *
+ * _strchr - main function
+ * @s: param
+ * @c: param
+ * Return: pnt
  */
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
-
+	
 	for (i = 0; i < n && src[i] != '\0'; i++)
 		dest[i] = src[i];
 	for ( ; i < n; i++)
 		dest[i] = '\0';
 
-	return dest;
+	return (dest);
 }
