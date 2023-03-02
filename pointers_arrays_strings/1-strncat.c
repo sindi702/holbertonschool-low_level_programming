7f294ee5616f6a9ba3592065ff42b718fd032efb
sindi702 <hamatajsindi@gmail.com>
2023-03-02 03:04:37 -0800

    pointers

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -9,13 +9,12 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	 int dest_len = strlen(dest);
-	 int i;
+	int dest_len = strlen(dest);
+	int i;
 
-	 for (i = 0 ; i < n && src[i] != '\0' ; i++)
-		 dest[dest_len + i] = src[i];
-	 dest[dest_len + i] = '\0';
-
-	 return dest;
+	for (i = 0 ; i < n && src[i] != '\0' ; i++)
+		dest[dest_len + i] = src[i];
+	dest[dest_len + i] = '\0';
+	return (dest);
 }
 
