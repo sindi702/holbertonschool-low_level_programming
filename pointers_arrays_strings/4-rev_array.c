dd4146df402d889478dfab0d3cfc09dab35f51ed
sindi702 <hamatajsindi@gmail.com>
2023-03-02 11:34:24 -0800

    pointers

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -7,12 +7,12 @@
   */
 void reverse_array(int *a, int n)
 {
-	int tmp , i;
+	int tmp, i;
 
-	for(i = 0; i < n / 2; i++)
+	for (i = 0; i < n / 2; i++)
 	{
 		tmp = a[i];
 		a[i] = a[n - 1 - i];
-		a[n - i - 1] = tmp;	
+		a[n - i - 1] = tmp;
 	}
 }
