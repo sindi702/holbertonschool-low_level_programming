450273f88d6ffbfb1f7062b4995b36dfd804beca
sindi702 <hamatajsindi@gmail.com>
2023-03-02 12:19:28 -0800

    pointers

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * leet - main function
+ * Return: returns sum
+ * @string: param
+ */
+char *leet(char *s)
+{
+	int i, j;
+	char lett[11] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+	char numbers[11] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; lett[j] != '\0'; j++)
+		{
+			if (s[i] == lett[j])
+			{
+				s[i] = numbers[j];
+			}
+		}
+	}
+	return (s);
+}
