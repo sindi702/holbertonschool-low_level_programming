0b131a18fc99656980b4c6328c693161fadabd11
sindi702 <hamatajsindi@gmail.com>
2023-03-02 01:15:50 -0800

    pointers

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -1,2 +1,21 @@
 #include "main.h"
+/**
+ * _strcat - main function
+ * @dest: param
+ * @src: param
+ * Return: dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i = 0 ;
+	int j;
 
+	while (destp[i]= 0)
+	{
+		i++;
+	}
+	for (j = 0; src[j] != 0; j++)
+		dest [i+j] = src [j];
+	dest[i+j] = '\0';
+	return (dest);
+}
