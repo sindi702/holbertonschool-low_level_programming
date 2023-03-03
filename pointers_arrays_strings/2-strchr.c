1b5dc83b9ee9dce29310a44a88fb1feadd2ecfe4
sindi702 <hamatajsindi@gmail.com>
2023-03-03 02:26:08 -0800

    pointer

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -8,11 +8,12 @@
  */
 char *_strchr(char *s, char c)
 {
-	int i, *size = 0;
+	int i, size = 0;
 	while (s[size]) != '\0')
 		size++;
 	if (s[size] == c)
 		return &s[size];
+
 	for (i = 0; s[i] != '\0'; i++)
 		if (s[i] == c)
 			return &s[i];
