972d29bc6d3c0a2df6dee60eca8da5033539b264
sindi702 <hamatajsindi@gmail.com>
2023-03-03 03:14:16 -0800

    pointer

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -9,15 +9,14 @@
 char *_strpbrk(char *s, char *accept)
 {
 	char *s_ptr, *a_ptr;
+
 	for (s_ptr = s; *s_ptr != '\0'; s_ptr++)
 	{
 		for (a_ptr = accept; *a_ptr != '\0'; a_ptr++)
 		{
 			if (*s_ptr == *a_ptr)
-			{
-				 return s_ptr;
-			}
+				return (s_ptr);
 		}
 	}
-	return NULL;
+	return (NULL);
 }
