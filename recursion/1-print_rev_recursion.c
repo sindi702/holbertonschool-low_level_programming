e38aeeac8b86086873f158aaa41c811330d7e06c
sindi702 <hamatajsindi@gmail.com>
2023-03-06 03:30:17 -0800

    recursion

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *
+ *
+ */
+void _print_rev_recursion(char *s)
+{
+	if(*s == '\0')
+	{
+		 _putchar('\n');
+		 return;
+	}
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
+}
