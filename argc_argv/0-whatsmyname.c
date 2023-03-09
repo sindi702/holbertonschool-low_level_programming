74a8a9c464e5aca45614e2c095e2ce451dfadcf7
sindi702 <hamatajsindi@gmail.com>
2023-03-09 01:54:46 -0800

    argv

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *
+ * main - main function
+ * @argc: parameter number
+ * @argv: all parameters
+ * Return: returns 0 if true
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
