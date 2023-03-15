c990c7585af176b1e0cb45e8f3888e04f8d69431
sindi702 <hamatajsindi@gmail.com>
2023-03-15 02:44:41 -0700

    malloca

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * **alloc_grid - create array function
+ * @width: string to concatenate
+ * @height: string to concatenate
+ * Return: ptr
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **ptr;
+	int i, j;
+
+	if (height <= 0)
+		return (NULL);
+	if (width <= 0)
+		return (NULL);
+	ptr = malloc(sizeof(int *) * height);
+	if (ptr == NULL)
+		return (NULL);
+	for (i = 0; i < height; i++)
+	{
+		ptr[i] = malloc(sizeof(int) * width);
+		if (ptr[i] == NULL)
+		{
+			while (i >= 0)
+			{
+				free(ptr[i]);
+				i--;
+			}
+			free(ptr);
+		}
+	}
+	for (i = 0; i < height; i++)
+	{
+		for (i = 0; i < height; i++)
+			ptr[i][j] = 0;
+	}
+	return (ptr);
+}
