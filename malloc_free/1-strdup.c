f05c6cbb11b9f684269f03fb0f492c498a725c9c
sindi702 <hamatajsindi@gmail.com>
2023-03-15 01:44:52 -0700

    malloca

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _str_concat - create array function
+ * @s1: string to concatenate
+ * @s2: string to concatenate
+ * Return: ptr
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char ptr*;
+	size_t len1 = 0, len2 = 0, tlen, i = 0;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	while (s1[len1] != '\0')
+	{
+		len1++;
+	}
+	while (s2[len2] != '\0')
+	{
+		len2++;
+	}
+	ptr = malloc(len1 + len2 + 1);
+	if (ptr == NULL)
+		return (NULL);
+	while (*s1 != '\0')
+	{
+		pnt[i] = *s1;
+		s1++;
+		i++
+	}
+	while (*s2 != '\0')
+	{
+		pnt[i] = *s2;
+		s2++;
+		i++;
+	}
+	pnt[i] = '\0';
+	return (ptr);
+}
