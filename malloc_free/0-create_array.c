05660d91a23b59430081c97af008da7359c02d7c
sindi702 <hamatajsindi@gmail.com>
2023-03-15 01:33:40 -0700

    malloca

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/*
+ * create_array - create array function
+ * @size: size
+ * @c: char to print
+ * Return: ptr
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *ptr;
+	size_t i = 0;
+
+	if (size == 0)
+		return (NULL);
+	ptr = malloc(size);
+	if (ptr == NULL)
+		return (NULL);
+	while (i < size)
+	{
+		ptr[i] = c;
+		i++;
+	}
+	return (ptr);
+}
