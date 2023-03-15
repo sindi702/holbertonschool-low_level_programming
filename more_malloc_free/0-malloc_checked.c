f03f2201582f2f30d4a6e90c44957ec99fb3e6ef
sindi702 <hamatajsindi@gmail.com>
2023-03-15 03:06:06 -0700

    malloca

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+/**
+ * malloc_checked -allocate memory using malloc
+ * @b: parameter
+ * Return: ptr
+ */
+void *malloc_checked(unsigned int b)
+{
+	char *ptr;
+
+	ptr = malloc (b);
+	if (ptr == NULL)
+		exit(98);
+	return (ptr);
+}
