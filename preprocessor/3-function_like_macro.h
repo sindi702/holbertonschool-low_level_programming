efee18982fa5bc96efa0a8151d13b5b5d2009e6c
sindi702 <hamatajsindi@gmail.com>
2023-03-17 01:54:55 -0700

    preprocesor

---
+++ preprocessor/3-function_like_macro.h
@@ -0,0 +1,9 @@
+#ifndef _FUNCTION_LIKE_MACRO
+#define _FUNCTION_LIKE_MACRO
+/**
+ * ABS(X) - computes the absolute value of a number
+ * @x: number that his value needs to be computed
+ */
+
+#define ABS(x) (((x) < 0) ? -(x) : (x))
+#endif
