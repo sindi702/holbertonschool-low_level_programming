32c32454f81675db3c0e98cf348d1f3208ba4d16
sindi702 <hamatajsindi@gmail.com>
2023-03-17 01:57:14 -0700

    preprocesor

--- preprocessor/1-pi.h
+++ preprocessor/1-pi.h
@@ -4,8 +4,5 @@
  * Desc: Header file that defines a macro named PI
  * as an abbrevation for the token 3.14159265359
  */
-
-
 #define PI 3.14159265359
-
 #endif
---
+++ preprocessor/4-sum.h
@@ -0,0 +1,10 @@
+#ifndef _SUM_H
+#define _SUM_H
+/**
+ * SUM - Sum of numbers
+ * @x: first parameter
+ * @y: second parameter
+ * Return: Always 0
+ */
+#define SUM(x, y) (x + y)
+#endif
