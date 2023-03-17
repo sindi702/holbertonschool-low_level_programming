5b626f8ca6231656bc72825affa947e2c9fbefc6
sindi702 <hamatajsindi@gmail.com>
2023-03-17 01:51:48 -0700

    preprocesor

---
+++ preprocessor/1-pi.h
@@ -0,0 +1,11 @@
+#ifndef _1_PI_H
+#define _1_PI_H
+/**
+ * Desc: Header file that defines a macro named PI
+ * as an abbrevation for the token 3.14159265359
+ */
+
+
+#define PI 3.14159265359
+
+#endif
