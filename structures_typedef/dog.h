d158d531c516ab8fa8a018d3fc7a3b08616f0275
sindi702 <hamatajsindi@gmail.com>
2023-03-20 02:52:52 -0700

    typedef

---
+++ structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - Short description
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
