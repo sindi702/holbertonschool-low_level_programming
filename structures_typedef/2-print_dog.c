b6427530cffb26a9b3f43a5b53e58cc11fe9d775
sindi702 <hamatajsindi@gmail.com>
2023-03-20 03:30:29 -0700

    typedef

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * print_dog function
+ * @name: name
+ * @owner: owner
+ * @age: age
+ * @d: struct
+ */
+void print_dog(struct dog *d)
+{
+	if(d)
+	{
+		if (d->name == NULL && d->owner == NULL)
+		{
+			d->name = "(nil)";
+			d->owner = "(owner)";
+		}
+		printf("Name: %s\n", d->name);
+		printf("Age: %f\n", d->age);
+		printf("Owner: %s\n", d->owner);
+	}
+}
