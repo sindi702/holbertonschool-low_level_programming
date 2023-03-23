e86a2f569eb7f4896ee630e9cad39b503d3dfe23
sindi702 <hamatajsindi@gmail.com>
2023-03-23 01:08:13 -0700

    typedef

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -6,12 +6,14 @@
  * @age: Second member
  * @owner: Third member
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 void print_dog(struct dog *d);
 void init_dog(struct dog *d, char *name, float age, char *owner);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 #endif
