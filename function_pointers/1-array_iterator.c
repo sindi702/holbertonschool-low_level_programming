447bac0837efeeb4256789176df2da370d8129ef
sindi702 <hamatajsindi@gmail.com>
2023-03-23 02:22:14 -0700

    functionpointers

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,23 @@
+#include "function_pointers.h"
+/**
+ * array_iterator - prints a name as is
+ * @array: first parameter
+ * @size: second parameter
+ * @action: third parameter
+ * Return: Nothing.
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	if (array && action)
+	{
+		i = 0;
+
+		while (i < size)
+		{
+			action(array[i]);
+			i++;
+		}
+	}
+}
