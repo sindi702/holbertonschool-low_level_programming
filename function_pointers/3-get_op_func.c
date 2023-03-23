c41678b2c7f59e04fe5718ac097e58ba8c862ecf
sindi702 <hamatajsindi@gmail.com>
2023-03-23 03:12:49 -0700

    functionpointers

---
+++ function_pointers/3-get_op_func.c
@@ -0,0 +1,29 @@
+#include "3-calc.h"
+#include <stdio.h>
+/**
+ * get_op_func - main fun
+ * @s: operators
+ * Return: return the value
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	int i;
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	i = 0;
+	while (ops[i].op)
+	{
+		if (*s == *ops[i].op)
+			return (ops[i].f);
+		i++;
+	}
+	printf("Error\n");
+	exit(99);
+	return (NULL);
+}
