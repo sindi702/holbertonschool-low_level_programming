143150f5f3c86bf566e0bf28eea4444321927cfb
sindi702 <hamatajsindi@gmail.com>
2023-03-24 03:29:28 -0700

    variadic

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,62 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *
+ *
+ */
+
+void print_char(va_list list)
+{
+	printf("%c" ,va_arg(list , int));
+}
+void print_int(va_list list)
+{
+	printf("%i" , va_arg(list , int));
+}
+void print_float(va_list list)
+{
+	printf("%f" , va_arg(list , double));
+}
+void print_string(va_list list)
+{
+	char *str ;
+	str = va_arg(list , char*);
+	if (str == NULL )
+		printf("(nil)");
+	printf("%s" , str);
+}
+void print_all(const char * const format, ...)
+{
+	int i = 0;
+	int j;
+	va_list arg_list;
+	char *sep = "";
+	print_t print_functions[] = {
+		{"c" , print_char},
+		{"i" , print_int},
+		{"f" , print_float},
+		{"s" , print_string}
+	};
+
+	va_start(arg_list , format);
+
+	while (format[i] != '\0' && format != NULL)
+	{
+		j = 0;
+		while (j < 4 && format[i] != *print_functions[j].f_type)
+			j++;
+		if (j < 4)
+		{
+			printf("%s" , sep);
+			print_functions[j].print(arg_list);
+			sep = ", ";
+		}
+		i++;
+	}
+	printf("\n");
+	va_end (arg_list);
+}
+
+
--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,8 +1,18 @@
 #ifndef VARIADIC_FUNCTION_H
 #define VARIADIC_FUNCTION_H
+#include <stdarg.h>
+typedef struct print_t
+{
+	char *f_type;
+	void (*print)(va_list list);
+} print_t;
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_char (va_list list);
+void print_int(va_list list);
+void print_float(va_list list);
+void print_string(va_list list);
 void print_all(const char * const format, ...);
 #endif
