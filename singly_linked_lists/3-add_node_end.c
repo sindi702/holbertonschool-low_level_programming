d77539c96bf3f36d6ceef072af170aa2c8d31c57
sindi702 <hamatajsindi@gmail.com>
2023-04-04 02:48:51 -0700

    single linked list

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -10,34 +10,31 @@
 list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new_node, *last_node;
-
-	 if (!head || !str)
-		    return (NULL);
-
-	 new_node = malloc(sizeof(list_t));
-	   if (!new_node)
-		   return (NULL);
-
-	    new_node->str = strdup(str);
-	     if (!new_node->str)
-	     {
-		      free(new_node);
-		       return (NULL);
-	     }
-
-	     new_node->len = strlen(str);
-	     new_node->next = NULL;
-
-	     if (!*head)
-	     {
-		      *head = new_node;
-		      return (new_node);
-	     }
-
-	      last_node = *head;
-	      while (last_node->next)
-		      last_node = last_node->next;
-
-	       last_node->next = new_node;
-	       return (new_node);
+	if (!head || !str)
+		return (NULL);
+	
+	new_node = malloc(sizeof(list_t));
+	if (!new_node)
+		return (NULL);
+	
+	new_node->str = strdup(str);
+	if (!new_node->str)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	new_node->len = strlen(str)
+		new_node->next = NULL;
+	
+	if (!*head)
+	{
+		*head = new_node;
+		return (new_node)
+	}
+	
+	last_node = *head;
+	while (last_node->next)
+		last_node = last_node->next;
+	last_node->next = new_node;
+	return (new_node);
 }
