f333421ffb760fa7063e7a7550ef085683b73f81
sindi702 <hamatajsindi@gmail.com>
2023-04-04 02:51:08 -0700

    single linked list

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -23,7 +23,7 @@ list_t *add_node_end(list_t **head, const char *str)
 		free(new_node);
 		return (NULL);
 	}
-	new_node->len = strlen(str)
+	new_node->len = strlen(str);
 		new_node->next = NULL;
 	
 	if (!*head)
