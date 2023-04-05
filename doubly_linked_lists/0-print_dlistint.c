46a802846a3cc73324a9f4fc79b4a8a9610ead5c
sindi702 <hamatajsindi@gmail.com>
2023-04-05 00:29:28 -0700

    double

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * print_dlistint - funk
+ * @h: head
+ * Return: return the nr of elemetn
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t count = 0;
+
+	if (h == NULL)
+		return (count);
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
