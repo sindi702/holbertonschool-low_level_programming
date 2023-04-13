73d0ca2c906220d5a0ed2334389b04d5d44bed25
sindi702 <hamatajsindi@gmail.com>
2023-04-13 02:55:02 -0700

    hashtable

---
+++ hash_tables/2-key_index.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "hash_tables.h"
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	if (key == NULL || size == 0)
+		return (0);
+	return (hash_djb2(key) % size);
+}
