e58a4697b3c94630401dfa152a11b624f755d2d1
sindi702 <hamatajsindi@gmail.com>
2023-04-13 02:47:51 -0700

    hashtable

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -3,7 +3,9 @@
 #include <stdio.h>
 #include "hash_tables.h"
 /**
- *
+ * hash_table_create - create a hash table
+ * @size: table size
+ * Return: table
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
