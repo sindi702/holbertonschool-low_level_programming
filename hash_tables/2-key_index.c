9653f1539de9dab40b0428126bbdbf4ec2253e66
sindi702 <hamatajsindi@gmail.com>
2023-04-13 02:56:33 -0700

    hashtable

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -2,7 +2,12 @@
 #include <string.h>
 #include <stdio.h>
 #include "hash_tables.h"
-
+/**
+ * key_index - create a hash table
+ * @size: array size
+ * @key: string to encrypt
+ * Return: index
+ */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	if (key == NULL || size == 0)
