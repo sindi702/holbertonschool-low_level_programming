5df1b223f7402f94721748a32d691ddd6058f8a1
sindi702 <hamatajsindi@gmail.com>
2023-04-14 01:47:14 -0700

    hashtable

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -10,12 +10,25 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *new;
+	hash_node_t *new, *curr_node;
 	unsigned long int idx;
 
 	if (key == NULL || ht == NULL)
 		return (0);
 	idx = key_index((const unsigned char *)key, ht->size);
+	curr_node = ht->array[index];
+
+	while(curr_node){
+		if (strcmp(curr_node->key, key) == 0)
+		{
+			free(curr_node->value);
+			curr_node->value = strdup(value);
+			if(new_node->value == NULL)
+				return (0);
+			return (1);
+		}
+		curr_node = curr_node->next;
+	}
 
 	new = malloc(sizeof(hash_node_t));
 
